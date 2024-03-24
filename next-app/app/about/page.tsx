870cd8a4b530aee04cdd6ef783f1281818fc7368
Mudassir <mudassirshahid84@gmail.com>
2024-03-25 03:02:52 +0500

    update link

--- next-app/app/about/page.tsx
+++ next-app/app/about/page.tsx
@@ -6,7 +6,7 @@ export default function AboutPage() {
   return (
     <>
     <h1 className="text-center bg-emerald-500 text-3xl p-3 text-black">About Page</h1>
-    <button onClick={() => router.push("/aboutcollege")} className="flex mx-auto py-3 bg-slate-600 my-5 p-4 rounded-md">About College</button>
+    <button onClick={() => router.push("/about/aboutcollege")} className="flex mx-auto py-3 bg-slate-600 my-5 p-4 rounded-md">About College</button>
     <button onClick={() => router.push("/services")} className="flex mx-auto py-3 bg-slate-600 my-5 p-4 rounded-md"> Go to Services Page</button>
     </>
   );
