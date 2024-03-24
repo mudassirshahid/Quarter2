a4bbf4c8bac2c144e59ddc01ba9dbc0fc22c6a95
Mudassir <mudassirshahid84@gmail.com>
2024-03-25 02:56:51 +0500

    change layout

--- next-app/app/services/page.tsx
+++ next-app/app/services/page.tsx
@@ -3,14 +3,17 @@ import ClassicMusicPage from './classic-music/page'
 
 function ServicePage() {
   return (
-    <div className='flex flex-wrap justify-center flex-col text-centerh-auto md:h-[10rem] w-full rounded-md items-center relative overflow-hidden mx-auto py-10 md-py-0'>
-      Service Page
+    <>
+    <h1 className="text-center bg-emerald-500 text-3xl p-3 text-black">Service Page</h1>
+
+    <div className='flex flex-wrap justify-center flex-col text-centerh-auto md:h-[0rem] w-full rounded-md items-center relative overflow-hidden mx-auto py-10 md-py-0'>
       <ClassicMusicPage 
-      title="ClassicMusicPageInner"
+      title="ServicesInner"
       para="some text"
       showH3={false}
       />
     </div>
+    </>
   )
 }
 
