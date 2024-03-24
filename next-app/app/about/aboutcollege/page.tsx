01a35f69c0e3ce2ddd4ad9e52dc952997a277ca6
Mudassir <mudassirshahid84@gmail.com>
2024-03-25 02:25:02 +0500

    add page

---
+++ next-app/app/about/aboutcollege/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function aboutCollege() {
+  return (
+    <div>
+     <h1 className="text-center bg-emerald-500 text-3xl p-3 text-black">About College Page</h1>
+    </div>
+  )
+}
+
+export default aboutCollege
