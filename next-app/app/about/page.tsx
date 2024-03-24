22246efab1c83ae6e3e8314a5260c20b4384e8ff
Mudassir <mudassirshahid84@gmail.com>
2024-03-25 02:57:18 +0500

    add button route

--- next-app/app/about/page.tsx
+++ next-app/app/about/page.tsx
@@ -6,7 +6,8 @@ export default function AboutPage() {
   return (
     <>
     <h1 className="text-center bg-emerald-500 text-3xl p-3 text-black">About Page</h1>
-    <button onClick={() => router.push("/services")} className="flex mx-auto py-3"> Go to Services Page</button>
+    <button onClick={() => router.push("/aboutcollege")} className="flex mx-auto py-3 bg-slate-600 my-5 p-4 rounded-md">About College</button>
+    <button onClick={() => router.push("/services")} className="flex mx-auto py-3 bg-slate-600 my-5 p-4 rounded-md"> Go to Services Page</button>
     </>
   );
 }
